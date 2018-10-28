c25904a16a39d4deb691a47d7aa1bacb05ebce10
Ismail Jalloh <ijalloh21@gmail.com>
2018-10-28 23:36:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(x)
+def fizzbuzz(int)
   if ((x % 5 == 0) && (x % 3 == 0))
     puts "fizzbuzz"
   elseif x % 5 == 0
